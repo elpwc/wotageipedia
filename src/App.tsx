926c17b854087183ec8aa3c5cc43d4d700bdb3cc
elpwc/wniko <elpwc@hotmail.com>
2021-11-03 00:44:19 +0800

    导入antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Layout, Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+const { Header, Footer, Sider, Content } = Layout;
+const { SubMenu } = Menu;
+
 
 interface P { }
 
-interface S { }
+interface S {
+  current: string;
+}
 
 class App extends React.Component<P, S> {
   constructor(props: P) {
     super(props);
-    this.state = {};
+    this.state = {
+      current: 'mail',
+    };
   }
-  
+
+  handleClick(e: any) {
+    console.log('click ', e);
+    this.setState({ current: e.key });
+  };
+
   render() {
     return (
-      <div className="App">
+      <Layout>
+        <Header>
+          <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+            <Menu.Item key="mail" icon={<MailOutlined />}>
+              Navigation One
+            </Menu.Item>
+            <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
+              Navigation Two
+            </Menu.Item>
+            <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Navigation Three - Submenu">
+              <Menu.ItemGroup title="Item 1">
+                <Menu.Item key="setting:1">Option 1</Menu.Item>
+                <Menu.Item key="setting:2">Option 2</Menu.Item>
+              </Menu.ItemGroup>
+              <Menu.ItemGroup title="Item 2">
+                <Menu.Item key="setting:3">Option 3</Menu.Item>
+                <Menu.Item key="setting:4">Option 4</Menu.Item>
+              </Menu.ItemGroup>
+            </SubMenu>
+            <Menu.Item key="alipay">
+              <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+                Navigation Four - Link
+              </a>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content>
 
-      </div>
+        </Content>
+        <Footer>
+
+        </Footer>
+      </Layout>
     );
   }
 }
