5f6f1214324858470a588601aeb96ac0b68f07b4
elpwc/wniko <elpwc@hotmail.com>
2022-01-30 20:38:13 +0800

    F: register page(developped on a train

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -5,6 +5,8 @@ import { Row, Col, Input, Button } from 'antd';
 import Updater from '../../utils/Updater';
 import './index.css';
 import { register } from '../../api/user';
+import Form from 'antd/lib/form/Form';
+import FormItem from 'antd/lib/form/FormItem';
 
 interface P {
   updater: Updater;
@@ -28,28 +30,62 @@ export default (props: P) => {
   return (
     <>
       <Row>
-        <Col span={DeviceStorage.value === 1 ? 24 : 18} offset={DeviceStorage.value === 1 ? 0 : 3} style={{ /*backgroundColor: 'white', boxShadow: '0 0 5px 0 gray',*/ paddingBottom: '100px' }}>
-          <Input
-            value={username}
-            onChange={e => {
-              setusername(e.target.value);
-            }}
-            placeholder={'用户名'}
-          />
-          <Input
-            value={password}
-            onChange={e => {
-              setpassword(e.target.value);
-            }}
-            placeholder={'密码'}
-          />
-          <Input
-            value={nick}
-            onChange={e => {
-              setnick(e.target.value);
-            }}
-            placeholder={'昵称'}
-          />
+        <Col
+          span={DeviceStorage.value === 1 ? 24 : 18}
+          offset={DeviceStorage.value === 1 ? 0 : 3}
+          style={{ /*backgroundColor: 'white', boxShadow: '0 0 5px 0 gray',*/ paddingBottom: '100px', display: 'flex', flexDirection: 'column', gap: '50px' }}
+        >
+          <Form>
+            <FormItem label={'用户名'}>
+              <Input
+                value={username}
+                onChange={e => {
+                  setusername(e.target.value);
+                }}
+                placeholder={'用户名'}
+              />
+            </FormItem>
+
+            <FormItem label={'密码'}>
+              <Input
+                value={password}
+                onChange={e => {
+                  setpassword(e.target.value);
+                }}
+                placeholder={'密码'}
+              />
+            </FormItem>
+
+            <FormItem label={'再输一遍密码'}>
+              <Input
+                value={password}
+                onChange={e => {
+                  setpassword(e.target.value);
+                }}
+                placeholder={'密码确认'}
+              />
+            </FormItem>
+
+            <FormItem label={'昵称'}>
+              <Input
+                value={nick}
+                onChange={e => {
+                  setnick(e.target.value);
+                }}
+                placeholder={'昵称'}
+              />
+            </FormItem>
+
+            <FormItem label={'用户名'}>
+              <Input
+                value={nick}
+                onChange={e => {
+                  setnick(e.target.value);
+                }}
+                placeholder={'昵称'}
+              />
+            </FormItem>
+          </Form>
           <Button
             onClick={() => {
               register(username, '', password, nick, 0, '', '').then(res => {
