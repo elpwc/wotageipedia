f719617c285c9b9ff064d053349298e5957846de
elpwc/wniko <elpwc@hotmail.com>
2022-02-17 20:42:06 +0800

    feat: 加入自动生成API请求

--- src/appconfig.ts
+++ src/appconfig.ts
@@ -1,8 +1,13 @@
 import { Lang } from './locales/langUtils';
 
+const inDebug = true;
+
 export default {
+  inDebug: inDebug,
   /** 启用的语言 */
   usingLanguages: [Lang.ja, Lang.zhcn],
   /** 服务器根目录 */
   root: '', //'/wotagei/'
+  /** API Base URL */
+  apiBaseURL: inDebug ? 'http://wotagei.elpwc.com/' : '/',
 };
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,11 +1,13 @@
-import axios from 'axios';
-import api from '../api/api';
+import axios, { AxiosRequestHeaders, Method } from 'axios';
 import cookie from 'react-cookies';
+import appconfig from '../appconfig';
+
+// 更新services里的接口的方法：npm run openapi
 
 axios.defaults.withCredentials = false;
 
 const service = axios.create({
-  baseURL: api.url_debug, //api.url,
+  baseURL: appconfig.apiBaseURL,
   timeout: 5000,
   responseType: 'json',
   withCredentials: true,
@@ -29,4 +31,40 @@ service.interceptors.request.use(
   }
 );
 
-export default service.request;
+/** 请求格式 */
+interface RequestOptions {
+  data?: object;
+  headers?: AxiosRequestHeaders;
+  method?: Method;
+  params?: object | URLSearchParams;
+  paramsSerializer?: (params: object) => string;
+  timeout?: number;
+  timeoutMessage?: string;
+}
+
+/** 返回格式 */
+interface RequestResponse<T = any> {
+  data: T;
+  response: Response;
+}
+
+/**
+ * 请求
+ * @param {string} url URL
+ * @param {RequestOptions} options Request option
+ * @returns {Promise<RequestResponse<T>>} Response
+ * @author wniko
+ */
+const request = <T = any>(url: string, options?: RequestOptions): Promise<RequestResponse<T>> => {
+  return service.request({
+    url,
+    method: options?.method,
+    data: options?.data,
+    params: options?.params,
+    paramsSerializer: options?.paramsSerializer,
+    timeout: options?.timeout,
+    timeoutErrorMessage: options?.timeoutMessage,
+  });
+};
+
+export default request;
