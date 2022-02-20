c1e0a6b4d7fe44bf70f1a2b10a094df94256e1bc
elpwc/wniko <elpwc@hotmail.com>
2022-02-21 02:05:19 +0800

    api: 错误拦截1

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -31,6 +31,45 @@ service.interceptors.request.use(
   }
 );
 
+/** 错误返回格式 */
+interface ErrorResponse {
+  errcode: number;
+  message: string;
+  showType?: number;
+  data?: object;
+}
+
+// 错误拦截
+service.interceptors.response.use(
+  res => {
+    return res;
+  },
+  err => {
+    const errResponse = err.response;
+    console.log('Oops! ', errResponse);
+
+    switch (errResponse.code) {
+      case 400:
+        break;
+      case 401:
+        break;
+      case 403:
+        break;
+      case 404:
+        break;
+      case 422:
+        break;
+      case 500:
+        break;
+      default:
+        break;
+    }
+
+
+    return errResponse?.data;
+  }
+)
+
 /** 请求格式 */
 interface RequestOptions {
   data?: object;
@@ -55,16 +94,25 @@ interface RequestResponse<T = any> {
  * @returns {Promise<RequestResponse<T>>} Response
  * @author wniko
  */
-const request = <T = any>(url: string, options?: RequestOptions): Promise<RequestResponse<T>> => {
-  return service.request({
-    url,
-    method: options?.method,
-    data: options?.data,
-    params: options?.params,
-    paramsSerializer: options?.paramsSerializer,
-    timeout: options?.timeout,
-    timeoutErrorMessage: options?.timeoutMessage,
+const request = <T = any>(url: string, options?: RequestOptions): Promise<T> => {
+  return new Promise((resolve: ((value: T) => void), reject: ((error: any) => void)) => {
+    service.request({
+      url,
+      method: options?.method,
+      data: options?.data,
+      params: options?.params,
+      paramsSerializer: options?.paramsSerializer,
+      timeout: options?.timeout,
+      timeoutErrorMessage: options?.timeoutMessage,
+    }).then((response: any) => {
+      // console.log(response);
+      resolve(response.data as T);
+    }).catch((err: ErrorResponse) => {
+      // console.log(err);
+      reject(err);
+    })
   });
+
 };
 
 export default request;
