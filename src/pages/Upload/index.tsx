e752e98c854dae5cac9c91853dcae1367409cb8c
elpwc/wniko <elpwc@hotmail.com>
2022-06-02 00:42:31 +0800

    feat: Upload init

--- src/pages/Upload/index.tsx
+++ src/pages/Upload/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { CurrentPageStorage, DeviceStorage, WinWidthStorage } from '../../dataStorage/storage';
 import { Navigate, useLocation, useNavigate, useParams } from 'react-router';
-import { Row, Col } from 'antd';
+import { Row, Col, Form, Input, Button } from 'antd';
 import Updater from '../../interfaces/Updater';
 import './index.css';
 import LangUtils from '../../locales/langUtils';
@@ -30,7 +30,46 @@ export default (props: P) => {
     <>
       <Row>
         <Col span={DeviceStorage.value === 1 ? 24 : 18} offset={DeviceStorage.value === 1 ? 0 : 3} style={{ /*backgroundColor: 'white', boxShadow: '0 0 5px 0 gray',*/ paddingBottom: '100px' }}>
-          upload
+          <Form>
+            <Form.Item label="技名CN" name="wazaname">
+              <Input />
+            </Form.Item>
+            <Form.Item label="技名别称CN" name="wazaname">
+              <Input />
+            </Form.Item>
+            <Form.Item label="技名JP" name="wazaname">
+              <Input />
+            </Form.Item>
+            <Form.Item label="技名别称JP" name="wazaname">
+              <Input />
+            </Form.Item>
+            <Form.Item label="来自" name="wazaname">
+              <Input />
+            </Form.Item>
+            <Form.Item label="链接" name="wazaname">
+              <Input />
+            </Form.Item>
+            <Form.Item label="开始时间" name="wazaname">
+              <Input />
+            </Form.Item>
+            <Form.Item label="本家" name="wazaname">
+              <Input />
+            </Form.Item>
+            <Form.Item label="系" name="wazaname">
+              <Input />
+            </Form.Item>
+            <Form.Item label="标签" name="wazaname">
+              <Input />
+            </Form.Item>
+            <Form.Item label="创立时间" name="wazaname">
+              <Input />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                添加
+              </Button>
+            </Form.Item>
+          </Form>
         </Col>
       </Row>
     </>
