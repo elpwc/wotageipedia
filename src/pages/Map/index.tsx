e1b9ab1dbf6ad820b650751cf6e496c1f3a46e73
elpwc/wniko <elpwc@hotmail.com>
2022-08-09 19:02:13 +0800

    Feat: map page

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -5,6 +5,7 @@ import { Row, Col } from 'antd';
 import Updater from '../../interfaces/Updater';
 import './index.css';
 import LangUtils from '../../locales/langUtils';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 
 interface P {
   updater: Updater;
@@ -29,7 +30,23 @@ export default (props: P) => {
     <>
       <Row>
         <Col span={DeviceStorage.value === 1 ? 24 : 18} offset={DeviceStorage.value === 1 ? 0 : 3} style={{ /*backgroundColor: 'white', boxShadow: '0 0 5px 0 gray',*/ paddingBottom: '100px' }}>
-          map
+
+            <MapContainer
+              center={[51.505, -0.09]}
+              zoom={13}
+              scrollWheelZoom={true}
+              style={{
+                width: '1000px',
+                height: '500px',
+              }}
+            >
+              <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+              <Marker position={[51.505, -0.09]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>
         </Col>
       </Row>
     </>
