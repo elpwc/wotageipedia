1a4c12f3ae1188b3d5bfa7f4e9431892be5bcf6f
elpwc/wniko <elpwc@hotmail.com>
2022-08-11 16:06:12 +0800

    Feat: map page, switch to original leaflet

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -5,9 +5,8 @@ import { Row, Col, Button } from 'antd';
 import Updater from '../../interfaces/Updater';
 import './index.css';
 import LangUtils from '../../locales/langUtils';
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
-import LeafletMap from './components/LeafletMap';
 import { Input, Space } from 'antd';
+import L, { LatLngExpression } from 'leaflet';
 
 const { Search } = Input;
 
@@ -23,16 +22,42 @@ export default (props: P) => {
   // let currentId: number = Number(params.wazaid);
 
   const [tmp, settmp]: [number, any] = useState(5);
+  const [position, setPosition]: [LatLngExpression | null, any] = useState(null);
+
+  let map: L.Map;
+  let mapLayer: L.Layer;
 
   useEffect(() => {
     // document.title = '';
     CurrentPageStorage.set('map');
     props.updater.setUpdate();
+
+    map = L.map('map', { attributionControl: false, zoomControl: false, maxBounds: L.latLngBounds(L.latLng(-100, -200), L.latLng(100, 100)) }).setView([51.505, -0.09], 13);
+
+    mapLayer =  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 7,
+      minZoom: 2,
+      tileSize: 200,
+      zoomOffset: 0,
+    });
+
+    
+    mapLayer.addTo(map);
+
+    // 注册map event
+    map.on('locationfound', e => {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    });
   }, []);
 
-  const L = LangUtils.selectLang();
+  const La = LangUtils.selectLang();
 
   const onSearch = () => {};
+
+  const onLocate = () => {
+    map.locate();
+  };
 
   return (
     <>
@@ -53,6 +78,7 @@ export default (props: P) => {
                 >
                   添加界隈
                 </Button>
+                <Button onClick={onLocate}>定位</Button>
               </div>
               <div className="listItems">
                 {Array.from(new Array(tmp).keys()).map(i => {
@@ -63,7 +89,31 @@ export default (props: P) => {
             </div>
           </div>
           <div className="rightCol">
-            <LeafletMap width="100%" height="600px" />
+            <div
+              id="map"
+              style={{
+                width: '100%',
+                height: '600px',
+              }}
+            ></div>
+            {/*
+            <MapContainer
+              center={[51.505, -0.09]}
+              zoom={13}
+              scrollWheelZoom={true}
+              style={{
+                width: '100%',
+                height: '600px',
+              }}
+            >
+              <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+              <Marker position={[51.505, -0.09]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>
+            <LeafletMap width="100%" height="600px" />*/}
           </div>
         </Col>
       </Row>
