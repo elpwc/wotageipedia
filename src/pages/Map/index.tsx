c218fe4f420e7ff37d786ee6f9991f6fb3cce7d1
elpwc/wniko <elpwc@hotmail.com>
2022-08-11 15:04:56 +0800

    Feat: list on map page

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from 'react';
 import { CurrentPageStorage, DeviceStorage, WinWidthStorage } from '../../dataStorage/storage';
 import { Navigate, useLocation, useNavigate, useParams } from 'react-router';
-import { Row, Col } from 'antd';
+import { Row, Col, Button } from 'antd';
 import Updater from '../../interfaces/Updater';
 import './index.css';
 import LangUtils from '../../locales/langUtils';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import LeafletMap from './components/LeafletMap';
+import { Input, Space } from 'antd';
+
+const { Search } = Input;
 
 interface P {
   updater: Updater;
@@ -18,6 +22,8 @@ export default (props: P) => {
 
   // let currentId: number = Number(params.wazaid);
 
+  const [tmp, settmp]: [number, any] = useState(5);
+
   useEffect(() => {
     // document.title = '';
     CurrentPageStorage.set('map');
@@ -26,27 +32,39 @@ export default (props: P) => {
 
   const L = LangUtils.selectLang();
 
+  const onSearch = () => {};
+
   return (
     <>
       <Row>
-        <Col span={DeviceStorage.value === 1 ? 24 : 18} offset={DeviceStorage.value === 1 ? 0 : 3} style={{ /*backgroundColor: 'white', boxShadow: '0 0 5px 0 gray',*/ paddingBottom: '100px' }}>
-
-            <MapContainer
-              center={[51.505, -0.09]}
-              zoom={13}
-              scrollWheelZoom={true}
-              style={{
-                width: '1000px',
-                height: '500px',
-              }}
-            >
-              <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-              <Marker position={[51.505, -0.09]}>
-                <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-              </Marker>
-            </MapContainer>
+        <Col
+          span={DeviceStorage.value === 1 ? 24 : 18}
+          offset={DeviceStorage.value === 1 ? 0 : 3}
+          style={{ /*backgroundColor: 'white', boxShadow: '0 0 5px 0 gray',*/ paddingBottom: '100px', display: 'flex', flexDirection: 'row' }}
+        >
+          <div className="leftCol">
+            <div className="listContainer">
+              <div className="searchContainer">
+                <Search placeholder="搜索界隈" onSearch={onSearch} style={{ width: 200 }} />
+                <Button
+                  onClick={() => {
+                    settmp(tmp + 1);
+                  }}
+                >
+                  添加界隈
+                </Button>
+              </div>
+              <div className="listItems">
+                {Array.from(new Array(tmp).keys()).map(i => {
+                  console.log(123, tmp);
+                  return <div className="listitem">test {i}</div>;
+                })}
+              </div>
+            </div>
+          </div>
+          <div className="rightCol">
+            <LeafletMap width="100%" height="600px" />
+          </div>
         </Col>
       </Row>
     </>
